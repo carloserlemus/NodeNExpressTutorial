f9e32e46e17a34699c84f740a90509bda2398831
carloserlemus <carlosriveralemus@gmail.com>
2020-04-11 06:13:11 -0700

    Added new route, updates tutorial notes, and began process for iterating in pug.

--- app.js
+++ app.js
@@ -1,10 +1,47 @@
 const express = require('express');
+const pug = require('pug');
 const app = express();
 
+// Load View Engine
+app.set('view engine', 'pug')
+
+// Home Route
 app.get('/', (req, res) => {
-    res.send('<h1> Hello World</h1>')
+    let articles = [
+        {
+            id: 1,
+            title: 'Article 1',
+            auther: 'X. Charles',
+            body:'This is the article body.'
+        },
+        {
+            id: 2,
+            title: 'Article 2',
+            auther: 'J. Smith',
+            body:'This is the article body.'
+        },
+        {
+            id: 3,
+            title: 'Article 3',
+            auther: 'H. Mccoy',
+            body:'This is the article body.'
+        }
+    ]
+
+    res.render('index', {
+        title: 'Hello',
+        articles: articles
+    })
 })
 
+// Add Route
+app.get('/articles/add', (req, res) => {
+    res.render('add', {
+        title: 'Add Article'
+    })
+})
+
+// Start Server
 app.listen(3000, () => {
     console.log('Smooth Sailing on port 3000...')
 })
